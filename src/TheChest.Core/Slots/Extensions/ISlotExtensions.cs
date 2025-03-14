e4aa31e7febf4c53582ae13d5e3d40084ee33d72
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2025-03-14 08:53:05 -0300

    Adds ISlotExtensions.Contains

---
+++ src/TheChest.Core/Slots/Extensions/ISlotExtensions.cs
@@ -0,0 +1,14 @@
+﻿using TheChest.Core.Slots.Interfaces;
+
+namespace TheChest.Core.Slots.Extensions
+{
+    public static class ISlotExtensions
+    {
+        public static bool Contains<T>(this ISlot<T> slot, T item)
+        {
+            return slot.Content?.Equals(
+                item ?? throw new ArgumentNullException(nameof(item))
+            ) ?? false;
+        }
+    }
+}
