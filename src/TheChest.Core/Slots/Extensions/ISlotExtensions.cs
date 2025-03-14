72dac1544495c1a2d6b4740dc716517071fd01e9
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2025-03-14 09:14:24 -0300

    Changes structure of ISlotExtensions.Contains<T>

--- src/TheChest.Core/Slots/Extensions/ISlotExtensions.cs
+++ src/TheChest.Core/Slots/Extensions/ISlotExtensions.cs
@@ -4,11 +4,21 @@ namespace TheChest.Core.Slots.Extensions
 {
     public static class ISlotExtensions
     {
+        /// <summary>
+        /// Checks if the slot contains the item.
+        /// </summary>
+        /// <typeparam name="T">Type of item to be checked inside the slot</typeparam>
+        /// <param name="item">Item to be checked</param>
+        /// <returns>Returns true if <paramref name="item"/> is equal to <see cref="ISlot{T}.Content"/> and both are not null</returns>
+        /// <exception cref="ArgumentNullException">When <paramref name="item"/> is null</exception>
         public static bool Contains<T>(this ISlot<T> slot, T item)
         {
-            return slot.Content?.Equals(
-                item ?? throw new ArgumentNullException(nameof(item))
-            ) ?? false;
+            item = item ?? throw new ArgumentNullException(nameof(item));
+
+            if(slot.IsEmpty) 
+                return false;
+            
+            return slot.Content!.Equals(item);
         }
     }
 }
