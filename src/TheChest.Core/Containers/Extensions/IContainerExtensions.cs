a750bf7a3c832be16633724e0e626b97e827777f
Franklin Bitencourt Dantas <franklin.bdantas@hotmail.com>
2025-04-01 08:09:37 -0300

    Adds Contains methods to Containers
    * Adds IContainer<T>.Contains
    * Adds IStackContainer<T>.Contains
    * Adds IContainerTests.contains
    * Adds IStackContainerTests.contains

---
+++ src/TheChest.Core/Containers/Extensions/IContainerExtensions.cs
@@ -0,0 +1,28 @@
+﻿using TheChest.Core.Containers.Interfaces;
+using TheChest.Core.Slots.Extensions;
+
+namespace TheChest.Core.Containers.Extensions
+{
+    public static class IContainerExtensions
+    {
+        /// <summary>
+        /// Checks if the container contains a item.
+        /// </summary>
+        /// <typeparam name="T">Type of item to be checked inside the container</typeparam>
+        /// <param name="item">Item to be checked</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">When <paramref name="item"/> is null</exception>
+        public static bool Contains<T>(this IContainer<T> container, T item)
+        {
+            item = item ?? throw new ArgumentNullException(nameof(item));
+            
+            for (var i = 0; i < container.Size; i++)
+            {
+                if (container[i].Contains(item))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
