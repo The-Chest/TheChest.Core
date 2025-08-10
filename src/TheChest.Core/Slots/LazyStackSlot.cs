90d29df1c249b3b59e53779d28cdb0599eae0369
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2025-08-10 12:23:52 -0300

    Adds Contains to LazyStackSlot

--- src/TheChest.Core/Slots/LazyStackSlot.cs
+++ src/TheChest.Core/Slots/LazyStackSlot.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using TheChest.Core.Slots.Interfaces;
 
 namespace TheChest.Core.Slots
@@ -16,9 +15,17 @@ namespace TheChest.Core.Slots
         private const string AMOUNT_BIGGER_THAN_MAXAMOUNT = "The item amount cannot be bigger than max amount";
 
         /// <summary>
+        /// The content inside the slot
+        /// </summary>
+        protected readonly T content;
+        /// <summary>
         /// The current amount of items inside the slot
         /// </summary>
         protected int stackAmount;
+        /// <summary>
+        /// The maximum amount of items that this slot can hold
+        /// </summary>
+        protected int maxStackAmount;
 
         /// <inheritdoc/>
         public virtual int StackAmount
@@ -38,12 +45,6 @@ namespace TheChest.Core.Slots
                 stackAmount = value;
             }
         }
-
-        /// <summary>
-        /// The maximum amount of items that this slot can hold
-        /// </summary>
-        protected int maxStackAmount;
-
         /// <inheritdoc/>
         public virtual int MaxStackAmount
         {
@@ -65,17 +66,8 @@ namespace TheChest.Core.Slots
 
         /// <inheritdoc/>
         public virtual bool IsFull => StackAmount == MaxStackAmount;
-
         /// <inheritdoc/>
         public virtual bool IsEmpty => StackAmount == 0;
-
-        /// <summary>
-        /// The content inside the slot
-        /// </summary>
-        protected readonly ICollection<T> content;
-
-        /// <inheritdoc/>
-        public virtual IReadOnlyCollection<T> Content => content.ToArray();
 
         /// <summary>
         /// Creates a basic Stack Slot with an amount and max amount
@@ -84,15 +76,50 @@ namespace TheChest.Core.Slots
         /// <param name="amount">The amount of <paramref name="currentItem"/> to be added</param>
         /// <param name="maxStackAmount">The maximum permited amount of <paramref name="currentItem"/> to be added</param>
         /// <exception cref="ArgumentOutOfRangeException"></exception>
-        public LazyStackSlot(T currentItem, int amount = 1, int maxStackAmount = 1)
+        public LazyStackSlot(T currentItem = default!, int amount = 1, int maxStackAmount = 1)
         {
             if (EqualityComparer<T>.Default.Equals(currentItem, default!))
                 amount = 0;
 
-            this.content = Enumerable.Repeat(currentItem, amount).ToArray();
-
+            this.content = currentItem;
             this.MaxStackAmount = maxStackAmount;
             this.StackAmount = amount;
+        }
+
+        /// <inheritdoc/>
+        /// <exception cref="ArgumentNullException">When <paramref name="item"/> is null</exception>
+        public bool Contains(T item, int amount)
+        {
+            item = item ?? throw new ArgumentNullException(nameof(item));
+            if (amount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(amount));
+
+            if (this.IsEmpty)
+                return false;
+            return item.Equals(this.content) && amount <= this.StackAmount;
+        }
+
+        /// <inheritdoc/>
+        [Obsolete("Use Contains(T item) or Contains(T item, int amount) instead")]
+        public bool Contains(params T[] items)
+        {
+            if (items.Length == 0 || items.Length > 1)
+                return false;
+            if (this.IsEmpty)
+                return false;
+
+            return this.content!.Equals(items[0]);
+        }
+
+        /// <inheritdoc/>
+        /// <exception cref="ArgumentNullException">When <paramref name="item"/> is null</exception>
+        public bool Contains(T item)
+        {
+            item = item ?? throw new ArgumentNullException(nameof(item));
+            if (this.IsEmpty)
+                return false;
+
+            return item.Equals(this.content);
         }
     }
 }
