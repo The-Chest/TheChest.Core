caedcae73441935ce5d2172afa263a4be5ae65e3
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2025-08-11 10:06:32 -0300

    Adds IStackSlotTests.contains_items

--- tests/TheChest.Core.Tests/Slots/IStackSlotTests/IStackSlotTests.contains_items.cs
+++ tests/TheChest.Core.Tests/Slots/IStackSlotTests/IStackSlotTests.contains_items.cs
@@ -21,5 +21,44 @@
             };
             Assert.That(() => slot.Contains(items), Throws.ArgumentNullException);
         }
+
+        [Test]
+        public void ContainsItems_ContainingOnlyOne_ReturnsFalse()
+        {
+            var slot = this.slotFactory.FullSlot(this.itemFactory.CreateDefault());
+            var items = new T[2]
+            {
+                this.itemFactory.CreateDefault(),
+                this.itemFactory.CreateRandom(),
+            };
+            var result = slot.Contains(items);
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void ContainsItems_ContainingNoItems_ReturnsFalse()
+        {
+            var slot = this.slotFactory.FullSlot(this.itemFactory.CreateDefault());
+            var items = new T[2]
+            {
+                this.itemFactory.CreateRandom(),
+                this.itemFactory.CreateRandom(),
+            };
+            var result = slot.Contains(items);
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void ContainsItems_AllItems_ReturnsTrue()
+        {
+            var slot = this.slotFactory.FullSlot(this.itemFactory.CreateDefault());
+            var items = new T[2]
+            {
+                this.itemFactory.CreateDefault(),
+                this.itemFactory.CreateDefault(),
+            };
+            var result = slot.Contains(items);
+            Assert.That(result, Is.True);
+        }
     }
 }
