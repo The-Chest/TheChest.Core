41db43facd04f1d7e01f917517dd0ef6f9c88482
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2025-09-20 13:43:00 -0300

    Adds ILazyStackSlotTests

---
+++ tests/TheChest.Core.Tests/Slots/ILazyStackSlotTests/ILazyStackSlotTests.contains_amount.cs
@@ -0,0 +1,69 @@
+﻿namespace TheChest.Core.Tests.Slots
+{
+    public partial class ILazyStackSlotTests<T>
+    {
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void ContainsAmount_InvalidAmount_ThrowsArgumentOutOfRangeException(int amount)
+        {
+            var item = this.itemFactory.CreateDefault();
+            var slot = this.slotFactory.EmptySlot();
+            Assert.That(
+                () => slot.Contains(item, amount), 
+                Throws.TypeOf<ArgumentOutOfRangeException>().And.Message.Contains("amount")
+             );
+        }
+
+        [Test]
+        public void ContainsAmount_MaxStackAmountSmallerThanSearchedAmount_ReturnsFalse()
+        {
+            var item = this.itemFactory.CreateDefault();
+            var amount = this.random.Next(1, 10);
+            var slot = this.slotFactory.WithItem(item, amount);
+
+            var paramItem = this.itemFactory.CreateDefault();
+            var paramAmount = amount + this.random.Next(1, 10);
+
+            Assert.That(slot.Contains(paramItem, paramAmount), Is.False);
+        }
+
+        [Test]
+        public void ContainsAmount_AmountSmallerThanSearchedAmount_ReturnsFalse()
+        {
+            var item = this.itemFactory.CreateDefault();
+            var amount = this.random.Next(1, 10);
+            var slot = this.slotFactory.WithItem(item, amount);
+
+            var paramItem = this.itemFactory.CreateDefault();
+            var paramAmount = this.random.Next(11, 20);
+
+            Assert.That(slot.Contains(paramItem, paramAmount), Is.False);
+        }
+
+        [Test]
+        public void ContainsAmount_AmountEqualThanSearchedAmount_ReturnsTrue()
+        {
+            var item = this.itemFactory.CreateDefault();
+            var amount = this.random.Next(1, 10);
+            var slot = this.slotFactory.WithItem(item, amount);
+
+            var paramItem = this.itemFactory.CreateDefault();
+            var paramAmount = amount;
+
+            Assert.That(slot.Contains(paramItem, paramAmount), Is.True);
+        }
+
+        [Test]
+        public void ContainsAmount_AmountBiggerThanSearchedAmount_ReturnsTrue()
+        {
+            var item = this.itemFactory.CreateDefault();
+            var amount = this.random.Next(11, 20);
+            var slot = this.slotFactory.WithItem(item, amount, 20);
+
+            var paramItem = this.itemFactory.CreateDefault();
+            var paramAmount = this.random.Next(1, 10);
+
+            Assert.That(slot.Contains(paramItem, paramAmount), Is.True);
+        }
+    }
+}
