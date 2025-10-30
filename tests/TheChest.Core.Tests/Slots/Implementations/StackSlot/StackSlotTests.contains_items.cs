d29add11551fd5d2017a2300ceff90ef0f333d90
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2025-10-29 23:35:20 -0300

    Adds StackSlotTests

---
+++ tests/TheChest.Core.Tests/Slots/Implementations/StackSlot/StackSlotTests.contains_items.cs
@@ -0,0 +1,17 @@
+﻿namespace TheChest.Core.Tests.Slots.Implementations
+{
+    public partial class StackSlotTests<T>
+    {
+        [Test]
+        public void ContainsItems_ParamsWithNull_ThrowsArgumentNullException()
+        {
+            var slot = this.slotFactory.FullSlot(this.itemFactory.CreateDefault());
+            var items = new T[2]
+            {
+                this.itemFactory.CreateDefault(),
+                default!
+            };
+            Assert.That(() => slot.Contains(items), Throws.ArgumentNullException);
+        }
+    }
+}
