84594bf7d9b8f2b80868cc8c7eacc24294c028c1
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2025-10-29 23:35:09 -0300

    Moves IStackSlotTests to Interface namespaces

---
+++ tests/TheChest.Core.Tests/Slots/Interfaces/IStackSlot/IStackSlotTests.cs
@@ -0,0 +1,24 @@
+﻿using TheChest.Core.Tests.Items.Interfaces;
+using TheChest.Core.Tests.Slots.Interfaces.Factories;
+
+namespace TheChest.Core.Tests.Slots.Interfaces
+{
+    public abstract partial class IStackSlotTests<T>
+    {
+        protected readonly IStackSlotFactory<T> slotFactory;
+        protected readonly IItemFactory<T> itemFactory;
+        protected Random random;
+
+        protected IStackSlotTests(IStackSlotFactory<T> slotFactory, IItemFactory<T> itemFactory)
+        {
+            this.slotFactory = slotFactory;
+            this.itemFactory = itemFactory;
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            random = new Random();
+        }
+    }
+}
