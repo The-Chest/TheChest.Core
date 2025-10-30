6e39537e5078fbccff4718bd282115e68d6e7398
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2025-10-29 22:38:05 -0300

    Adds StackContainer unit tests

---
+++ tests/TheChest.Core.Tests/Containers/Implementations/StackContainer/StackContainerTests.cs
@@ -0,0 +1,12 @@
+﻿using TheChest.Core.Tests.Containers.Interfaces;
+
+namespace TheChest.Core.Tests.Containers.Implementations
+{
+    public partial class StackContainerTests<T> : IStackContainerTests<T>
+    {
+        public StackContainerTests(IStackContainerFactory<T> containerFactory, ISlotItemFactory<T> itemFactory) :
+            base(containerFactory, itemFactory)
+        {
+        }
+    }
+}
