1ed8f9a5db9d5f85cddf03b482f9f76ac5d152f8
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2025-10-29 23:26:00 -0300

    Adds SlotTests

---
+++ tests/TheChest.Core.Tests/Slots/Implementations/Slot/SlotTests.contains.cs
@@ -0,0 +1,12 @@
+﻿namespace TheChest.Core.Tests.Slots.Implementations
+{
+    public partial class SlotTests<T>
+    {
+        [Test]
+        public void Contains_NullItem_ThrowsArgumentNullException()
+        {
+            var slot = this.slotFactory.EmptySlot();
+            Assert.Throws<ArgumentNullException>(() => slot.Contains(default!));
+        }
+    }
+}
---
+++ tests/TheChest.Core.Tests/Slots/Implementations/Slot/SlotTests.cs
@@ -0,0 +1,14 @@
+﻿using TheChest.Core.Tests.Items.Interfaces;
+using TheChest.Core.Tests.Slots.Interfaces;
+using TheChest.Core.Tests.Slots.Interfaces.Factories;
+
+namespace TheChest.Core.Tests.Slots.Implementations
+{
+    public partial class SlotTests<T> : ISlotTests<T>
+    {
+        public SlotTests(ISlotFactory<T> slotFactory, IItemFactory<T> itemFactory) : 
+            base(slotFactory, itemFactory)
+        {
+        }
+    }
+}
