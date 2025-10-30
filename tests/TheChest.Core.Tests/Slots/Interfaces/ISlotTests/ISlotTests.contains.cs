2159a9dce428d25176f2613a79a1cfc3ee31b4b9
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2025-10-29 23:19:17 -0300

    Moves ISlotTests to Interface namespaces

--- tests/TheChest.Core.Tests/Slots/Interfaces/ISlotTests/ISlotTests.contains.cs
+++ tests/TheChest.Core.Tests/Slots/Interfaces/ISlotTests/ISlotTests.contains.cs
@@ -6,7 +6,7 @@
         public void Contains_NullItem_ThrowsArgumentNullException()
         {
             var slot = this.slotFactory.EmptySlot();
-            Assert.Throws<ArgumentNullException>(() => slot.Contains(default));
+            Assert.Throws<ArgumentNullException>(() => slot.Contains(default!));
         }
 
         [Test]
---
+++ tests/TheChest.Core.Tests/Slots/Interfaces/ISlotTests/ISlotTests.cs
@@ -0,0 +1,17 @@
+﻿using TheChest.Core.Tests.Items.Interfaces;
+using TheChest.Core.Tests.Slots.Interfaces.Factories;
+
+namespace TheChest.Core.Tests.Slots
+{
+    public abstract partial class ISlotTests<T>
+    {
+        protected readonly ISlotFactory<T> slotFactory;
+        protected readonly IItemFactory<T> itemFactory;
+
+        protected ISlotTests(ISlotFactory<T> slotFactory, IItemFactory<T> itemFactory)
+        {
+            this.slotFactory = slotFactory;
+            this.itemFactory = itemFactory;
+        }
+    }
+}
