0f2a9cb56a2c4e129c8efe70a4b6d3c2708e0552
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2025-12-02 13:23:50 -0300

    Adds TypeConditionAttribute to unit tests

---
+++ tests/TheChest.Core.Tests/Configurations/Attributes/TypeConditionAttribute.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+
+namespace TheChest.Core.Tests.Configurations.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    internal abstract class TypeConditionAttribute : NUnitAttribute, IApplyToTest
+    {
+        protected abstract bool ShouldIgnore(Type type);
+
+        protected virtual string Reason => "Condition not met.";
+
+        public void ApplyToTest(Test test)
+        {
+            var fixtureType = test.TypeInfo?.Type;
+            if (fixtureType == null)
+                return;
+
+            if (!fixtureType.IsGenericType)
+                return;
+
+            var t = fixtureType.GetGenericArguments()[0];
+
+            if (this.ShouldIgnore(t))
+            {
+                test.RunState = RunState.Ignored;
+            }
+        }
+    }
+}
