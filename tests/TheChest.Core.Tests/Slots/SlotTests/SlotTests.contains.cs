ef2ca372375d9bde066289fa41ae9fd518827d78
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2025-12-02 19:33:39 -0300

    Fixes SlotTests.contains

--- tests/TheChest.Core.Tests/Slots/SlotTests/SlotTests.contains.cs
+++ tests/TheChest.Core.Tests/Slots/SlotTests/SlotTests.contains.cs
@@ -1,22 +1,24 @@
-﻿namespace TheChest.Core.Tests.Slots.SlotTests
+﻿using TheChest.Core.Tests.Configurations.Attributes;
+
+namespace TheChest.Core.Tests.Slots.SlotTests
 {
     public partial class SlotTests<T>
     {
         [Test]
+        [IgnoreIfValueType]
         public void Contains_NullItem_ThrowsArgumentNullException()
         {
-            Assume.That(!typeof(T).IsValueType, "Test applies only to reference types.");
             var slot = this.slotFactory.EmptySlot();
             Assert.That(() => slot.Contains(default!), Throws.ArgumentNullException);
         }
 
         [Test]
-        public void Contains_DefaultValue_ReturnsFalse()
+        [IgnoreIfReferenceType]
+        public void Contains_DefaultValue_DefaultItem_ReturnsTrue()
         {
-            Assume.That(typeof(T).IsValueType, "Test applies only to value types.");
-            var slot = this.slotFactory.EmptySlot();
+            var slot = this.slotFactory.FullSlot(default!);
             var result = slot.Contains(default!);
-            Assert.That(result, Is.False);
+            Assert.That(result, Is.True);
         }
     }
 }
