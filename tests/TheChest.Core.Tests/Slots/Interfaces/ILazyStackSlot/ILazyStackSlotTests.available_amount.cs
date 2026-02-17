48c7cfdbdd8bf39d470244eced91e9748ac13140
Franklin Bitencourt Dantas <franklin.bdantas@hotmail.com>
2026-02-17 18:11:23 -0300

    Improves AvailableAmount unit tests

--- tests/TheChest.Core.Tests/Slots/Interfaces/ILazyStackSlot/ILazyStackSlotTests.available_amount.cs
+++ tests/TheChest.Core.Tests/Slots/Interfaces/ILazyStackSlot/ILazyStackSlotTests.available_amount.cs
@@ -5,7 +5,10 @@
         [Test]
         public void AvailableAmount_EmptySlot_ReturnsMaxAmount()
         {
-            var slot = this.slotFactory.WithItem(this.itemFactory.CreateDefault(), 0);
+            var amount = this.random.Next(1, 10);
+            var maxAmount = this.random.Next(10, 20);
+            var slot = this.slotFactory.WithItem(this.itemFactory.CreateDefault(), amount, maxAmount);
+            
             Assert.That(slot.AvailableAmount, Is.EqualTo(slot.MaxAmount));
         }
 
@@ -13,14 +16,17 @@
         public void AvailableAmount_FullSlot_ReturnsZero()
         {
             var slot = this.slotFactory.FullSlot(this.itemFactory.CreateDefault());
+            
             Assert.That(slot.AvailableAmount, Is.Zero);
         }
 
         [Test]
         public void AvailableAmount_SlotWithItem_ReturnsMaxAmountLessAmount()
         {
-            var amount = 5;
-            var slot = this.slotFactory.WithItem(this.itemFactory.CreateDefault(), amount);
+            var amount = this.random.Next(1, 10);
+            var maxAmount = this.random.Next(10, 20);
+            var slot = this.slotFactory.WithItem(this.itemFactory.CreateDefault(), amount, maxAmount);
+            
             Assert.That(slot.AvailableAmount, Is.EqualTo(slot.MaxAmount - slot.Amount));
         }
     }
