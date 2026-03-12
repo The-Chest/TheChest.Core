f119e78422f6760eb5ba37ffd52332ad498223c1
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2026-03-12 14:14:35 -0300

    Adds LazyStackContainer constructors

--- src/TheChest.Core/Containers/LazyStackContainer.cs
+++ src/TheChest.Core/Containers/LazyStackContainer.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Linq;
 using TheChest.Core.Containers.Interfaces;
+using TheChest.Core.Slots;
 using TheChest.Core.Slots.Interfaces;
 
 namespace TheChest.Core.Containers
@@ -18,22 +18,69 @@ namespace TheChest.Core.Containers
         /// <inheritdoc/>
         public virtual int Size => this.slots.Length;
         /// <inheritdoc/>
-        public virtual bool IsFull => this.slots.All(x => x.IsFull);
-        /// <inheritdoc/>
-        public virtual bool IsEmpty => this.slots.All(x => x.IsEmpty);
+        public virtual bool IsFull
+        {
+            get
+            {
+                for (int i = 0; i < this.slots.Length; i++)
+                {
+                    if (!this.slots[i].IsFull)
+                        return false;
+                }
 
+                return true;
+            }
+        }
+        /// <inheritdoc/>
+        public virtual bool IsEmpty
+        {
+            get
+            {
+                for (int i = 0; i < this.slots.Length; i++)
+                {
+                    if (!this.slots[i].IsEmpty)
+                        return false;
+                }
+
+                return true;
+            }
+        }
+        public LazyStackContainer() : this(20, 1) { }
+        public LazyStackContainer(int size, int maxStackSize) : this(new (T item, int amount)[size], maxStackSize) { }
+        public LazyStackContainer((T item, int amount)[] items, int maxAmount)
+        {
+            if (items == null)
+                throw new ArgumentNullException(nameof(items));
+            if (maxAmount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(maxAmount));
+
+            var lazySlots = new ILazyStackSlot<T>[items.Length];
+
+            for (int i = 0; i < items.Length; i++) 
+            {
+                var (item, amount) = items[i];
+                if (item is null)
+                    throw new ArgumentNullException(nameof(items), $"Item at index {i} is null.");
+                if (amount <= 0)
+                    throw new ArgumentOutOfRangeException(nameof(items), $"Amount at index {i} must be greater than zero.");
+
+                lazySlots[i] = new LazyStackSlot<T>(item, amount, maxAmount);
+            }
+
+            this.slots = lazySlots;
+        }
         /// <summary>
         /// Creates a Container with <see cref="ILazyStackSlot{T}"/> implementation
         /// </summary>
         /// <param name="slots">An array of <see cref="ILazyStackSlot{T}"/></param>
-        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentNullException">When <paramref name="slots"/> is <see langword="null"/></exception>
         public LazyStackContainer(ILazyStackSlot<T>[] slots)
         {
             this.slots = slots ?? throw new ArgumentNullException(nameof(slots));
         }
 
         /// <inheritdoc/>
-        /// <exception cref="ArgumentNullException">When <paramref name="item"/> is null</exception>
+        /// <exception cref="ArgumentNullException">When <paramref name="item"/> is <see langword="null"/></exception>
         public virtual bool Contains(T item)
         {
             if (item is null)
@@ -48,7 +95,7 @@ namespace TheChest.Core.Containers
             return false;
         }
         /// <inheritdoc/>
-        /// <exception cref="ArgumentNullException">When <paramref name="item"/> is null</exception>
+        /// <exception cref="ArgumentNullException">When <paramref name="item"/> is <see langword="null"/></exception>
         /// <exception cref="ArgumentOutOfRangeException">When <paramref name="amount"/> zero or smaller</exception>
         public virtual bool Contains(T item, int amount)
         {
