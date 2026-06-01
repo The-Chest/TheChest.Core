726a477f4bfc9d2ad07729a1cc633f9066b50189
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2026-06-01 11:05:09 -0300

    Moves IContainerTests.contains_amount to ContainerTests

--- tests/Containers/ContainerTests/ContainerTests.contains_amount.cs
+++ tests/Containers/ContainerTests/ContainerTests.contains_amount.cs
@@ -67,5 +67,88 @@ namespace TheChest.Core.Tests.Containers.ContainerTests
             var amount = this.random.Next(1, size + 1);
             Assert.That(slot.Contains(default!, amount), Is.True);
         }
+
+        [Test(Description = "ContainsAmount method returns false when the container is empty, regardless of the item.")]
+        [Category("Contains With Amount")]
+        [Category("Valid Parameters")]
+        [Category("Behavior")]
+        [Category("Failure")]
+        public void ContainsAmount_EmptyContainer_ReturnsFalse()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var container = this.containerFactory.Empty(size);
+
+            var item = this.itemFactory.CreateDefault();
+            Assert.That(container.Contains(item), Is.False);
+        }
+
+        [Test(Description = "ContainsAmount method returns false when the item is not found in the container, regardless of the amount.")]
+        [Category("Contains With Amount")]
+        [Category("Valid Parameters")]
+        [Category("Behavior")]
+        [Category("Failure")]
+        public void ContainsAmount_NotFoundItem_ReturnsFalse()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var items = this.itemFactory.CreateManyRandom(size);
+            var container = this.containerFactory.WithItemsShuffled(size, items);
+
+            var item = this.itemFactory.CreateDefault();
+            var amount = this.random.Next(1, size + 1);
+            Assert.That(container.Contains(item, amount), Is.False);
+        }
+
+        [Test(Description = "ContainsAmount method returns false when the total amount of the item in the container is smaller than the searched amount.")]
+        [Category("Contains With Amount")]
+        [Category("Valid Parameters")]
+        [Category("Result")]
+        [Category("Failure")]
+        public void ContainsAmount_AmountSmallerThanSearchedAmount_ReturnsFalse()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var item = this.itemFactory.CreateDefault();
+            var items = this.itemFactory.CreateManyRandom(size - 1)
+                .Append(item).ToArray();
+            var container = this.containerFactory.WithItemsShuffled(size, items);
+
+            var paramItem = this.itemFactory.CreateDefault();
+            var amount = this.random.Next(2, size + 1);
+            Assert.That(container.Contains(paramItem, amount), Is.False);
+        }
+
+        [Test(Description = "ContainsAmount method returns true when the total amount of the item in the container is equal to the searched amount.")]
+        [Category("Contains With Amount")]
+        [Category("Valid Parameters")]
+        [Category("Result")]
+        [Category("Success")]
+        public void ContainsAmount_AmountEqualThanSearchedAmount_ReturnsTrue()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var halfSize = size / 2;
+            var items = this.itemFactory.CreateManyRandom(halfSize).ToList();
+            items.AddRange(this.itemFactory.CreateMany(halfSize));
+            var container = this.containerFactory.WithItemsShuffled(size, items.ToArray());
+
+            var paramItem = this.itemFactory.CreateDefault();
+            Assert.That(container.Contains(paramItem, halfSize), Is.True);
+        }
+
+        [Test(Description = "ContainsAmount method returns true when the total amount of the item in the container is bigger than the searched amount.")]
+        [Category("Contains With Amount")]
+        [Category("Valid Parameters")]
+        [Category("Result")]
+        [Category("Success")]
+        public void ContainsAmount_AmountBiggerThanSearchedAmount_ReturnsTrue()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var item = this.itemFactory.CreateRandom();
+            var items = this.itemFactory.CreateMany(size - 1)
+                .Append(item).ToArray();
+            var container = this.containerFactory.WithItemsShuffled(size, items.ToArray());
+
+            var paramItem = this.itemFactory.CreateDefault();
+            var amount = this.random.Next(1, size - 1);
+            Assert.That(container.Contains(paramItem, amount), Is.True);
+        }
     }
 }
