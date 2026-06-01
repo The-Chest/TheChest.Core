d85f0f1f4db99e6cd17dd41b3ec32604ac76c72e
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2026-06-01 10:45:50 -0300

    Moves IContainerTests.contains to ContainerTests

--- tests/Containers/ContainerTests/ContainerTests.contains.cs
+++ tests/Containers/ContainerTests/ContainerTests.contains.cs
@@ -43,5 +43,52 @@ namespace TheChest.Core.Tests.Containers.ContainerTests
             var slot = this.containerFactory.Full(20, item!);
             Assert.That(slot.Contains(item: default!), Is.True);
         }
+
+
+        [Test(Description = "Contains method throws an ArgumentNullException when a null item is passed.")]
+        [Category("Contains")]
+        [Category("Valid Parameters")]
+        [Category("Result")]
+        [Category("Failure")]
+        public void Contains_EmptyContainer_ReturnsFalse()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var container = this.containerFactory.Empty(size);
+
+            var item = this.itemFactory.CreateDefault();
+            Assert.That(container.Contains(item), Is.False);
+        }
+
+        [Test(Description = "Contains method returns false when all items in the container are different from the parameter.")]
+        [Category("Contains")]
+        [Category("Valid Parameters")]
+        [Category("Result")]
+        [Category("Failure")]
+        public void Contains_AllItemsDifferentFromParam_ReturnsFalse()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var item = this.itemFactory.CreateDefault();
+            var container = this.containerFactory.Full(size, item);
+
+            var paramItem = this.itemFactory.CreateRandom();
+            Assert.That(container.Contains(paramItem), Is.False);
+        }
+
+        [Test(Description = "Contains method returns true when at least one item in the container is equal to the parameter.")]
+        [Category("Contains")]
+        [Category("Valid Parameters")]
+        [Category("Result")]
+        [Category("Success")]
+        public void Contains_OneItemEqualsToParam_ReturnsTrue()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var item = this.itemFactory.CreateDefault();
+            var items = this.itemFactory.CreateManyRandom(size - 1)
+                .Append(item).ToArray();
+            var container = this.containerFactory.WithItemsShuffled(size, items);
+
+            var paramItem = this.itemFactory.CreateDefault();
+            Assert.That(container.Contains(paramItem), Is.True);
+        }
     }
 }
