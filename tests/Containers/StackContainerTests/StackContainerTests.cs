91aa1d805ea2a5225f217c8de4eda57ee7ebeb57
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2026-06-12 10:23:18 -0300

    Removes IStackContainerTests

--- tests/Containers/StackContainerTests/StackContainerTests.cs
+++ tests/Containers/StackContainerTests/StackContainerTests.cs
@@ -1,4 +1,6 @@
-﻿using TheChest.Core.Tests.Common.Items.ReferenceType;
+﻿using TheChest.Core.Tests.Common.Configurations;
+using TheChest.Core.Tests.Common.Items.Interfaces;
+using TheChest.Core.Tests.Common.Items.ReferenceType;
 using TheChest.Core.Tests.Common.Items.ValueType;
 using TheChest.Core.Tests.Containers.Interfaces;
 using TheChest.Core.Tests.Factories.Containers;
@@ -11,15 +13,27 @@ namespace TheChest.Core.Tests.Containers.StackContainerTests
     [TestFixture(typeof(TestItem))]
     [TestFixture(typeof(TestStructItem))]
     [TestFixture(typeof(TestEnumItem))]
-    public partial class StackContainerTests<T> : IStackContainerTests<T>
+    public partial class StackContainerTests<T> : BaseTest<T>
     {
+        protected readonly IStackContainerFactory<T> containerFactory;
+        protected readonly IItemFactory<T> itemFactory;
+
+        protected const int MIN_SIZE_TEST = 10;
+        protected const int MAX_SIZE_TEST = 20;
+
+        protected const int MIN_STACK_SIZE_TEST = 10;
+        protected const int MAX_STACK_SIZE_TEST = 20;
+
         public StackContainerTests() :
             base(
                 container => {
                     container.Register<IStackSlotFactory<T>, StackSlotFactory<StackSlot<T>, T>>();
                     container.Register<IStackContainerFactory<T>, StackContainerFactory<StackContainer<T>, T>>();
                 }
-            ) 
-        { }
+            )
+        {
+            this.containerFactory = this.configurations.Resolve<IStackContainerFactory<T>>();
+            this.itemFactory = this.configurations.Resolve<IItemFactory<T>>();
+        }
     }
 }
