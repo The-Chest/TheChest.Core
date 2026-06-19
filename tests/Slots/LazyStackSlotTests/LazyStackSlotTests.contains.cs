46ec6e7c26787c541facddc7b56ea1aac9c34bfe
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2026-06-19 13:21:07 -0300

    Moves ILazyStackSlotTests.Contains to LazyStackSlotTests

--- tests/Slots/LazyStackSlotTests/LazyStackSlotTests.contains.cs
+++ tests/Slots/LazyStackSlotTests/LazyStackSlotTests.contains.cs
@@ -1,31 +1,110 @@
-﻿using TheChest.Core.Tests.Common.Configurations.Attributes;
+﻿using NUnit.Framework.Internal;
+using TheChest.Core.Tests.Common.Configurations.Attributes;
 
 namespace TheChest.Core.Tests.Slots.LazyStackSlotTests
 {
     public partial class LazyStackSlotTests<T>
     {
         [Test]
+        [Description("Contains throws an ArgumentNullException when a null item is passed for reference types.")]
+        [Category("Contains")]
+        [Category("Behavior")]
+        [Category("Exception")]
+        [Category("Reference Type")]
         [IgnoreIfValueType]
         public void Contains_NullItem_ThrowsArgumentNullException()
         {
-            var slot = this.slotFactory.Empty();
-            Assert.That(() => slot.Contains(default!), Throws.ArgumentNullException);
+            var stackSize = this.GenerateStackSize();
+            var slot = this.slotFactory.Empty(0, stackSize);
+
+            Assert.That(
+                () => slot.Contains(default!), 
+                Throws.ArgumentNullException.And.Property("ParamName").EqualTo("item")
+            );
         }
 
         [Test]
+        [Description("Contains returns false when the default value is passed and the slot is empty.")]
+        [Category("Contains")]
+        [Category("Result")]
+        [Category("Failure")]
+        [Category("Value Type")]
         [IgnoreIfReferenceType]
         public void Contains_DefaultValue_ReturnsFalseIfEmpty()
         {
-            var slot = this.slotFactory.Empty();
-            Assert.That(slot.Contains(default!), Is.False);
+            var stackSize = this.GenerateStackSize();
+            var slot = this.slotFactory.Empty(0, stackSize);
+
+            var contains = slot.Contains(default!);
+
+            Assert.That(contains, Is.False);
         }
 
         [Test]
+        [Description("Contains returns false when the slot is empty, regardless of the item passed.")]
+        [Category("Contains")]
+        [Category("Result")]
+        [Category("Failure")]
+        public void Contains_EmptySlot_ReturnsFalse()
+        {
+            var stackSize = this.GenerateStackSize();
+            var slot = this.slotFactory.Empty(0, stackSize);
+
+            var item = this.itemFactory.CreateDefault();
+            var contains = slot.Contains(item);
+
+            Assert.That(contains, Is.False);
+        }
+
+        [Test]
+        [Description("Contains returns false when a different item is passed than the one contained in the slot.")]
+        [Category("Contains")]
+        [Category("Result")]
+        [Category("Failure")]
+        public void Contains_DifferentItem_ReturnsFalse()
+        {
+            var stackSize = this.GenerateStackSize();
+            var randomItem = this.itemFactory.CreateRandom();
+            var slot = this.slotFactory.Full(randomItem, stackSize);
+
+            var item = this.itemFactory.CreateDefault();
+            var contains = slot.Contains(item); 
+            
+            Assert.That(contains, Is.False);
+        }
+
+        [Test]
+        [Description("Contains returns true when the same item is passed that is contained in the slot.")]
+        [Category("Contains")]
+        [Category("Result")]
+        [Category("Success")]
+        public void Contains_ContainsItem_ReturnsTrue()
+        {
+            var stackSize = this.GenerateStackSize();
+            var sameItem = this.itemFactory.CreateDefault();
+            var slot = this.slotFactory.Full(sameItem, stackSize);
+
+            var item = this.itemFactory.CreateDefault();
+            var contains = slot.Contains(item);
+
+            Assert.That(contains, Is.True);
+        }
+
+        [Test]
+        [Description("Contains returns true when the default value is passed and the slot is full.")]
+        [Category("Contains")]
+        [Category("Result")]
+        [Category("Success")]
+        [Category("Value Type")]
         [IgnoreIfReferenceType]
         public void Contains_DefaultValue_ReturnsTrueIfFull()
         {
-            var slot = this.slotFactory.Full(default!);
-            Assert.That(slot.Contains(default!), Is.True);
+            var stackSize = this.GenerateStackSize();
+            var slot = this.slotFactory.Full(default!, stackSize);
+
+            var contains = slot.Contains(default!);
+
+            Assert.That(contains, Is.True);
         }
     }
 }
--- tests/Slots/LazyStackSlotTests/LazyStackSlotTests.contains_amount.cs
+++ tests/Slots/LazyStackSlotTests/LazyStackSlotTests.contains_amount.cs
@@ -1,46 +1,156 @@
 ﻿using TheChest.Core.Tests.Common.Configurations.Attributes;
+using TheChest.Core.Tests.Common.NUnit.TestCases;
 
 namespace TheChest.Core.Tests.Slots.LazyStackSlotTests
 {
     public partial class LazyStackSlotTests<T>
     {
-        [TestCase(0)]
-        [TestCase(-1)]
+        [WrongAmount]
+        [Description("Contains throws an ArgumentOutOfRangeException when the amount is less than or equal to zero.")]
+        [Category("ContainsAmount")]
+        [Category("Behavior")]
+        [Category("Exception")]
         public void ContainsAmount_InvalidAmount_ThrowsArgumentOutOfRangeException(int amount)
         {
+            var stackSize = this.GenerateStackSize();
+            var slot = this.slotFactory.Empty(0, stackSize);
+
             var item = this.itemFactory.CreateDefault();
-            var slot = this.slotFactory.Empty();
             Assert.That(
                 () => slot.Contains(item, amount), 
-                Throws.TypeOf<ArgumentOutOfRangeException>().And.Message.Contains("amount")
+                Throws.TypeOf<ArgumentOutOfRangeException>().And.Property("ParamName").EqualTo("amount")
              );
         }
 
         [Test]
+        [Description("Contains throws an ArgumentNullException when a null item is passed for reference types.")]
+        [Category("ContainsAmount")]
+        [Category("Behavior")]
+        [Category("Exception")]
+        [Category("Reference Type")]
         [IgnoreIfValueType]
         public void ContainsAmount_NullItem_ThrowsArgumentNullException()
         {
-            var slot = this.slotFactory.Empty();
+            var stackSize = this.GenerateStackSize();
+            var slot = this.slotFactory.Empty(0, stackSize);
+
             Assert.That(
                 () => slot.Contains(default!, 1),
-                Throws.TypeOf<ArgumentNullException>().And.Message.Contains("item")
+                Throws.TypeOf<ArgumentNullException>().And.Property("ParamName").EqualTo("item")
              );
         }
 
         [Test]
+        [Description("Contains returns false when the default value is passed and the slot is empty.")]
+        [Category("ContainsAmount")]
+        [Category("Result")]
+        [Category("Failure")]
+        [Category("Value Type")]
         [IgnoreIfReferenceType]
         public void ContainsAmount_NullItem_ReturnFalseIfEmpty()
         {
-            var slot = this.slotFactory.Empty();
-            Assert.That(slot.Contains(default!, 1), Is.False);
+            var stackSize = this.GenerateStackSize();
+            var slot = this.slotFactory.Empty(0, stackSize);
+
+            var amount = this.random.Next(1, stackSize);
+            var contains = slot.Contains(default!, amount);
+
+            Assert.That(contains, Is.False);
         }
 
         [Test]
+        [Description("Contains returns true when the default value is passed and the slot is full.")]
+        [Category("ContainsAmount")]
+        [Category("Result")]
+        [Category("Success")]
+        [Category("Value Type")]
         [IgnoreIfReferenceType]
         public void ContainsAmount_NullItem_ReturnTrueIfFull()
         {
-            var slot = this.slotFactory.Full(default!);
-            Assert.That(slot.Contains(default!, 1), Is.True);
+            var stackSize = this.GenerateStackSize();            
+            var slot = this.slotFactory.Full(default!, stackSize);
+
+            var amount = this.random.Next(1, stackSize);
+            var contains = slot.Contains(default!, amount);
+
+            Assert.That(contains, Is.True);
+        }
+
+
+        [Test]
+        [Description("Contains returns false when the slot is empty, regardless of the item passed.")]
+        [Category("ContainsAmount")]
+        [Category("Result")]
+        [Category("Failure")]
+        public void ContainsAmount_MaxStackAmountSmallerThanSearchedAmount_ReturnsFalse()
+        {
+            var stackSize = this.GenerateStackSize();
+            var amount = this.random.Next(1, stackSize);
+            var item = this.itemFactory.CreateDefault();
+            var slot = this.slotFactory.WithItem(item, amount, stackSize);
+
+            var paramItem = this.itemFactory.CreateDefault();
+            var paramAmount = this.random.Next(1, stackSize + amount);
+            var contains = slot.Contains(paramItem, paramAmount);
+
+            Assert.That(contains, Is.False);
+        }
+
+        [Test]
+        [Description("Contains returns false when the amount in the slot is smaller than the searched amount, even if the items match.")]
+        [Category("ContainsAmount")]
+        [Category("Result")]
+        [Category("Failure")]
+        public void ContainsAmount_AmountSmallerThanSearchedAmount_ReturnsFalse()
+        {
+            var stackSize = this.GenerateStackSize();
+            var amount = this.random.Next(1, stackSize);
+            var item = this.itemFactory.CreateDefault();
+            var slot = this.slotFactory.WithItem(item, amount, stackSize);
+
+            var paramItem = this.itemFactory.CreateDefault();
+            var paramAmount = this.random.Next(stackSize, stackSize * 2);
+            var contains = slot.Contains(paramItem, paramAmount);
+
+            Assert.That(contains, Is.False);
+        }
+
+        [Test]
+        [Description("Contains returns true when the amount in the slot is equal to the searched amount and the items match.")]
+        [Category("ContainsAmount")]
+        [Category("Result")]
+        [Category("Success")]
+        public void ContainsAmount_AmountEqualThanSearchedAmount_ReturnsTrue()
+        {
+            var stackSize = this.GenerateStackSize();
+            var amount = this.random.Next(1, stackSize);
+            var item = this.itemFactory.CreateDefault();
+            var slot = this.slotFactory.WithItem(item, amount, stackSize);
+
+            var paramItem = this.itemFactory.CreateDefault();
+            var paramAmount = amount;
+            var contains = slot.Contains(paramItem, paramAmount);
+
+            Assert.That(contains, Is.True);
+        }
+
+        [Test]
+        [Description("Contains returns true when the amount in the slot is bigger than the searched amount and the items match.")]
+        [Category("ContainsAmount")]
+        [Category("Result")]
+        [Category("Success")]
+        public void ContainsAmount_AmountBiggerThanSearchedAmount_ReturnsTrue()
+        {
+            var item = this.itemFactory.CreateDefault();
+            var stackSize = this.GenerateStackSize();
+            var amount = this.random.Next(stackSize / 2, stackSize);
+            var slot = this.slotFactory.WithItem(item, amount, stackSize);
+
+            var paramItem = this.itemFactory.CreateDefault();
+            var paramAmount = this.random.Next(1, amount);
+            var contains = slot.Contains(paramItem, paramAmount);
+
+            Assert.That(contains, Is.True);
         }
     }
 }
