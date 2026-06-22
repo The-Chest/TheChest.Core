04e37d6bb928e87c16ef90914ebe760effb53aec
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2026-06-22 13:02:25 -0300

    Improves SlotTests adding category

--- tests/Slots/SlotTests/SlotTests.cs
+++ tests/Slots/SlotTests/SlotTests.cs
@@ -1,18 +1,27 @@
-﻿using TheChest.Core.Tests.Common.Items.ReferenceType;
+﻿using TheChest.Core.Tests.Common.Configurations;
+using TheChest.Core.Tests.Common.Items.Interfaces;
+using TheChest.Core.Tests.Common.Items.ReferenceType;
 using TheChest.Core.Tests.Common.Items.ValueType;
 using TheChest.Core.Tests.Factories.Slots;
 using TheChest.Core.Tests.Factories.Slots.Interfaces;
-using TheChest.Core.Tests.Slots.Interfaces.ISlotTests;
 
 namespace TheChest.Core.Tests.Slots.SlotTests
 {
+    [Category("Slot")]
     [TestFixture(typeof(TestItem))]
     [TestFixture(typeof(TestStructItem))]
     [TestFixture(typeof(TestEnumItem))]
-    public partial class SlotTests<T> : ISlotTests<T>
+    public partial class SlotTests<T> : BaseTest<T>
     {
+        protected readonly ISlotFactory<T> slotFactory;
+        protected readonly IItemFactory<T> itemFactory;
+
         public SlotTests() : base(
             container => container.Register<ISlotFactory<T>, SlotFactory<Slot<T>,T>>()
-        ) { }
+        )
+        {
+            this.slotFactory = this.configurations.Resolve<ISlotFactory<T>>();
+            this.itemFactory = this.configurations.Resolve<IItemFactory<T>>();
+        }
     }
 }
--- tests/Slots/SlotTests/SlotTests.is_empty.cs
+++ tests/Slots/SlotTests/SlotTests.is_empty.cs
@@ -1,23 +1,34 @@
-﻿namespace TheChest.Core.Tests.Slots.Interfaces.ISlotTests
+﻿namespace TheChest.Core.Tests.Slots.SlotTests
 {
-    public partial class ISlotTests<T>
+    public partial class SlotTests<T>
     {
         [Test]
+        [Description("IsEmpty returns true for a slot with value type content.")]
+        [Category("IsEmpty")]
+        [Category("Property")]
         public void IsEmpty_CurrentItemDefault_ReturnsTrue()
         {
             var slot = this.slotFactory.Empty();
+
             Assert.That(slot.IsEmpty, Is.True);
         }
 
         [Test]
+        [Description("IsEmpty returns false for a slot with content.")]
+        [Category("IsEmpty")]
+        [Category("Property")]
         public void IsEmpty_WithCurrentItem_ReturnsFalse()
         {
             var item = this.itemFactory.CreateDefault();
             var slot = this.slotFactory.Full(item);
+
             Assert.That(slot.IsEmpty, Is.False);
         }
 
         [Test]
+        [Description("IsEmpty returns false for a full slot.")]
+        [Category("IsEmpty")]
+        [Category("Property")]
         public void IsEmpty_IsFull_ReturnsFalse()
         {
             var item = this.itemFactory.CreateDefault();
